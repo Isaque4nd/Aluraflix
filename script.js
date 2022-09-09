3143d2dc42f865f108c170e94aa87a1a94001cca
Isaque Ribeiro de Andrade <andradeisq1.7@gmail.com>
2022-09-09 20:14:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var listaFilmes = [" https://assistironline.net/wp-content/uploads/2020/10/assistir-Yesterday-2019-online.jpg", " https://play-lh.googleusercontent.com/em2griqrHoxmxEss_WM5Fi2iqSEKrEfLNAltjX54lREOR0nz0du__KuSi2bA_YNjS4w", " https://sm.ign.com/ign_br/screenshot/default/pixels-regposter-brazil_p1ja.jpg", " https://ingresso-a.akamaihd.net/img/cinema/cartaz/7766-cartaz.jpg", " https://upload.wikimedia.org/wikipedia/pt/3/33/Chamber_of_Secrets.jpg", " https://br.web.img2.acsta.net/medias/nmedia/18/93/88/04/20282944.jpg", " https://upload.wikimedia.org/wikipedia/pt/7/7b/Harry_Potter_C%C3%A1lice_Fogo_2004.jpg", " https://cdnsecakmi.kaltura.com/api_v3/index.php/service/thumbAsset/action/serve/thumbAssetId/1_ljei24wh/ks/", " https://cdnsecakmi.kaltura.com/api_v3/index.php/service/thumbAsset/action/serve/thumbAssetId/1_4g450d79/ks/", " https://upload.wikimedia.org/wikipedia/pt/thumb/6/64/Harry_Potter_Deathly_Hallows_2010.jpg/250px-Harry_Potter_Deathly_Hallows_2010.jpg", " https://upload.wikimedia.org/wikipedia/pt/3/3a/Harry_Potter_and_the_Deathly_Hallows_-_Part_2.jpg"];
+
+for (var i = 0; i < listaFilmes.length; i++) {;
+    document.write("<img src=" + listaFilmes[i] + ">");
+}
+
